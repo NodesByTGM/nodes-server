4e605580d12b3430febd12101181576c7f136840
Elijah Soladoye <41167893+shodown96@users.noreply.github.com>
2024-03-14 10:01:18 +0100

    Update users.router.ts

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { allUsersContoller, profileController } from '../controllers';
+import { allUsersContoller, profileController, profileUpdateController } from '../controllers';
 import { authenticate } from '../middlewares';
 
 const router = Router();
 
 router.get('/profile', authenticate, profileController);
-router.put('/profile', authenticate, profileController);
+router.put('/profile', authenticate, profileUpdateController);
 router.get('/', allUsersContoller);
 
 export default router;
